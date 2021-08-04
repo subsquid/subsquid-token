9c27bdba25ec510dbf1465a172148750dee4097e
sachushaji <sachushajiabraham@gmail.com>
2021-08-04 16:51:34 +0530

    add eth and ropsten config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,8 +16,31 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.2",
+const config: HardhatUserConfig ={
+  solidity: 
+  {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.ETH_KEY || ""]
+    }
+  },
+    etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: process.env.ETHERSCAN_API_KEY,
+    }
 };
+
 
 export default config;
