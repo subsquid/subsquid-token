d8fc6b09acd3e87e42faf339eeaa26a8d03727a8
sachushaji <sachushajiabraham@gmail.com>
2021-08-04 13:56:37 +0530

    add subsquid contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.0",
+  solidity: "0.8.2",
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Subsquid = await hre.ethers.getContractFactory("Subsquid");
+  const subsquid = await Subsquid.deploy();
 
-  await greeter.deployed();
+  await subsquid.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Subsquid deployed to:", subsquid.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
