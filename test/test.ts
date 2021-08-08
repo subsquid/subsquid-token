c3130b2451a9487e7558059f8b1c8e123dbe7afb
sachushaji <sachushajiabraham@gmail.com>
2021-08-08 21:58:00 +0530

    fix subsquid base test cases

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,28 @@
-const { expect } = require("chai");
+const { expect, use } = require("chai");
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+let SubsquidContractFactory:any , subsquidInstance:any;
+let owner:any;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Subsquid ERC20 Token", async function () {
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+    SubsquidContractFactory = await ethers.getContractFactory("Subsquid");
+    subsquidInstance =  await upgrades.deployProxy(SubsquidContractFactory, [],  { kind: 'uups' });
+    await subsquidInstance.deployed();
+  });
+  it("Should return the new subsquid token once it's changed", async function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    console.log("Box deployed to:", subsquidInstance.address);
+    expect(await subsquidInstance.decimals()).to.equal(18);
+    expect(await subsquidInstance.symbol()).to.equal("SQD");
+    expect(await subsquidInstance.balanceOf(owner.address)).to.equal("1000000000000000000000000000");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // const setGreetingTx = await subsquid.setGreeting("Hola, mundo!");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await subsquid.greet()).to.equal("Hola, mundo!");
   });
 });
