cea717c410a9fff15713b8a352f023df6f217b55
sachushaji <sachushajiabraham@gmail.com>
2021-08-09 22:56:58 +0530

    burn token successfully

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,15 @@
 import chai, {expect} from "chai";
 import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
+import { BigNumber } from "ethers";
 
 chai.use(solidity);
 
 let SubsquidContractFactory:any , subsquidInstance:any;
 let owner:any, addr1:any;
-const amount = 10000;
-const maxCap = "1000000000000000000000000000";
+const AMOUNT = 10000;
+const MAX_CAP = "1000000000000000000000000000";
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 describe("Subsquid ERC20 Token", async function () {
   before(async () => {
@@ -20,21 +22,32 @@ describe("Subsquid ERC20 Token", async function () {
   it("Should successfully create a subsquid token", async function () {
     expect(await subsquidInstance.decimals()).to.equal(18);
     expect(await subsquidInstance.symbol()).to.equal("SQD");
-    expect(await subsquidInstance.balanceOf(owner.address)).to.equal(maxCap);
+    expect(await subsquidInstance.balanceOf(owner.address)).to.equal(MAX_CAP);        
     expect(await subsquidInstance.owner()).to.equal(owner.address);
   });
 
   it("Should successfully be able to transfer token from one account to other",async function(){
-     await expect(subsquidInstance.transfer(addr1.address, amount)).to.emit(subsquidInstance, 'Transfer')
-     .withArgs(owner.address,addr1.address, amount);
-    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(amount);
+     await expect(subsquidInstance.transfer(addr1.address, AMOUNT)).to.emit(subsquidInstance, 'Transfer')
+     .withArgs(owner.address,addr1.address, AMOUNT);
+    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(AMOUNT);
   })
 
   it("Should successfully be able to pause and unpause a contract",async function(){
     await expect(subsquidInstance.pause()).to.emit(subsquidInstance, 'Paused') .withArgs(owner.address);
-    await expect( subsquidInstance.transfer(addr1.address, amount)).to.be.revertedWith('Pausable: paused')
+    await expect( subsquidInstance.transfer(addr1.address, AMOUNT)).to.be.revertedWith('Pausable: paused')
     await expect(subsquidInstance.unpause()).to.emit(subsquidInstance, 'Unpaused') .withArgs(owner.address);
-    await subsquidInstance.transfer(addr1.address, amount)
-    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(2* amount);
+    await subsquidInstance.transfer(addr1.address, AMOUNT)
+    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(2* AMOUNT);
+  })
+
+  it("Initialize should be callable once",async function(){
+    await expect(subsquidInstance.initialize()).to.be.revertedWith('Initializable: contract is already initialized')
+  })
+
+  it("Should successfully be able burn tokens",async function(){
+    const totalSupply = await subsquidInstance.totalSupply()
+    await expect(subsquidInstance.burn(AMOUNT)).to.emit(subsquidInstance, 'Transfer') .withArgs(owner.address, ZERO_ADDRESS, AMOUNT);
+    const newSupply = await subsquidInstance.totalSupply()
+    expect(BigNumber.from(newSupply)).to.equal(totalSupply.sub(AMOUNT));
   })
 });
