ac42ebbe3432319d7fb05282521622391487f32f
sachushaji <sachushajiabraham@gmail.com>
2021-08-09 23:17:35 +0530

    transfer from tests

--- test/test.ts
+++ test/test.ts
@@ -6,21 +6,21 @@ import { BigNumber } from "ethers";
 chai.use(solidity);
 
 let SubsquidContractFactory:any , subsquidInstance:any;
-let owner:any, addr1:any;
+let owner:any, addr1:any, addr2:any, addr3:any;
 const AMOUNT = 10000;
 const MAX_CAP = "1000000000000000000000000000";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-describe("Subsquid ERC20 Token", async function () {
+describe("Subsquid ERC20 Token Basic Tests", async function () {
   before(async () => {
-    [owner,addr1] = await ethers.getSigners();
+    [owner,addr1,addr2] = await ethers.getSigners();
     SubsquidContractFactory = await ethers.getContractFactory("Subsquid");
     subsquidInstance =  await upgrades.deployProxy(SubsquidContractFactory, [],  { kind: 'uups' });
     await subsquidInstance.deployed();
-    console.log("Subsquid deployed to:", subsquidInstance.address);
   });
   it("Should successfully create a subsquid token", async function () {
     expect(await subsquidInstance.decimals()).to.equal(18);
+    expect(await subsquidInstance.name()).to.equal("Subsquid");
     expect(await subsquidInstance.symbol()).to.equal("SQD");
     expect(await subsquidInstance.balanceOf(owner.address)).to.equal(MAX_CAP);        
     expect(await subsquidInstance.owner()).to.equal(owner.address);
@@ -49,5 +49,14 @@ describe("Subsquid ERC20 Token", async function () {
     await expect(subsquidInstance.burn(AMOUNT)).to.emit(subsquidInstance, 'Transfer') .withArgs(owner.address, ZERO_ADDRESS, AMOUNT);
     const newSupply = await subsquidInstance.totalSupply()
     expect(BigNumber.from(newSupply)).to.equal(totalSupply.sub(AMOUNT));
+  })
+
+  it("should successFully add allowance and be able to transfer amounts",async function(){
+     await expect(subsquidInstance.increaseAllowance(addr2.address,AMOUNT)).to.emit(subsquidInstance, 'Approval') 
+    .withArgs(owner.address, addr2.address, AMOUNT);
+    expect(await subsquidInstance.balanceOf(addr2.address)).to.equal(0);
+    await expect(subsquidInstance.connect(addr2).transferFrom(owner.address, addr2.address, AMOUNT)).to.emit(subsquidInstance, 'Transfer')
+     .withArgs(owner.address,  addr2.address, AMOUNT);
+     expect(await subsquidInstance.balanceOf(addr2.address)).to.equal(AMOUNT);
   })
 });
