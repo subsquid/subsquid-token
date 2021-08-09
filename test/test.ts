55f8cacc489d23c01db796fc8d52bfd9ca7433a7
sachushaji <sachushajiabraham@gmail.com>
2021-08-09 19:59:00 +0530

    add pause test

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,13 @@
-const { expect, use } = require("chai");
+import chai, {expect} from "chai";
 import { ethers } from "hardhat";
+import { solidity } from "ethereum-waffle";
+
+chai.use(solidity);
 
 let SubsquidContractFactory:any , subsquidInstance:any;
 let owner:any, addr1:any;
+const amount = 10000;
+const maxCap = "1000000000000000000000000000";
 
 describe("Subsquid ERC20 Token", async function () {
   before(async () => {
@@ -10,19 +15,28 @@ describe("Subsquid ERC20 Token", async function () {
     SubsquidContractFactory = await ethers.getContractFactory("Subsquid");
     subsquidInstance =  await upgrades.deployProxy(SubsquidContractFactory, [],  { kind: 'uups' });
     await subsquidInstance.deployed();
+    console.log("Subsquid deployed to:", subsquidInstance.address);
   });
   it("Should successfully create a subsquid token", async function () {
-
-    console.log("Box deployed to:", subsquidInstance.address);
     expect(await subsquidInstance.decimals()).to.equal(18);
     expect(await subsquidInstance.symbol()).to.equal("SQD");
-    expect(await subsquidInstance.balanceOf(owner.address)).to.equal("1000000000000000000000000000");
+    expect(await subsquidInstance.balanceOf(owner.address)).to.equal(maxCap);
+    expect(await subsquidInstance.owner()).to.equal(owner.address);
   });
 
   it("Should successfully be able to transfer token from one account to other",async function(){
-    const amount = 10000;
     const transferToken = await subsquidInstance.transfer(addr1.address, amount)
     await transferToken.wait();
     expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(amount);
+  })
+
+  it("Should successfully be able to pause and unpause a contract",async function(){
+    const pauseContract = await subsquidInstance.pause();
+    await pauseContract.wait()
+    await expect( subsquidInstance.transfer(addr1.address, amount)).to.be.revertedWith('Pausable: paused')
+    const unPauseContract = await subsquidInstance.unpause();
+    await unPauseContract.wait()
+    await subsquidInstance.transfer(addr1.address, amount)
+    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(2* amount);
   })
 });
