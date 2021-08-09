0cceb4110e91a0f3f67fb90826fd8e118ee789cf
sachushaji <sachushajiabraham@gmail.com>
2021-08-09 12:16:17 +0000

    change initilaizer variable

--- contracts/Subsquid.sol
+++ contracts/Subsquid.sol
@@ -8,8 +8,9 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+
 contract Subsquid is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
-    function initialize() initializer public {
+    function initialize() public initializer {
         __ERC20_init("Subsquid", "SQD");
         __ERC20Burnable_init();
         __Pausable_init();
