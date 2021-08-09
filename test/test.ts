2729fc68dc98a7b1baf87e622170bcd10ef50e97
sachushaji <sachushajiabraham@gmail.com>
2021-08-09 19:33:30 +0530

    add token transfer test

--- test/test.ts
+++ test/test.ts
@@ -2,27 +2,27 @@ const { expect, use } = require("chai");
 import { ethers } from "hardhat";
 
 let SubsquidContractFactory:any , subsquidInstance:any;
-let owner:any;
+let owner:any, addr1:any;
 
 describe("Subsquid ERC20 Token", async function () {
-  beforeEach(async () => {
-    [owner] = await ethers.getSigners();
+  before(async () => {
+    [owner,addr1] = await ethers.getSigners();
     SubsquidContractFactory = await ethers.getContractFactory("Subsquid");
     subsquidInstance =  await upgrades.deployProxy(SubsquidContractFactory, [],  { kind: 'uups' });
     await subsquidInstance.deployed();
   });
-  it("Should return the new subsquid token once it's changed", async function () {
+  it("Should successfully create a subsquid token", async function () {
 
     console.log("Box deployed to:", subsquidInstance.address);
     expect(await subsquidInstance.decimals()).to.equal(18);
     expect(await subsquidInstance.symbol()).to.equal("SQD");
     expect(await subsquidInstance.balanceOf(owner.address)).to.equal("1000000000000000000000000000");
-
-    // const setGreetingTx = await subsquid.setGreeting("Hola, mundo!");
-
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await subsquid.greet()).to.equal("Hola, mundo!");
   });
+
+  it("Should successfully be able to transfer token from one account to other",async function(){
+    const amount = 10000;
+    const transferToken = await subsquidInstance.transfer(addr1.address, amount)
+    await transferToken.wait();
+    expect(await subsquidInstance.balanceOf(addr1.address)).to.equal(amount);
+  })
 });
