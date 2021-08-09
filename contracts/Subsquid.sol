d34c434823c2d11ba4dbadf5e8b7c8ecf5e0a6cf
sachushaji <sachushajiabraham@gmail.com>
2021-08-10 00:42:33 +0530

    Add Natspac comments to the contract

--- contracts/Subsquid.sol
+++ contracts/Subsquid.sol
@@ -9,7 +9,14 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 
-contract Subsquid is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+/// @title Subsquid ERC20 Token
+/// @author Subsquid Team
+/// @notice You can use this contract for investing in subquid ecosystem
+/// @dev The contract is based on openzepplin upgradable ERC20 standards using UUPS upgradability mechanism
+contract Subsquid is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
+PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+
+    /// @notice initialiser function which will only called once upon contract creation
     function initialize() public initializer {
         __ERC20_init("Subsquid", "SQD");
         __ERC20Burnable_init();
@@ -20,18 +27,24 @@ contract Subsquid is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
+   /// @notice Pauses contract transfers callable only by admin
     function pause() public onlyOwner {
         _pause();
     }
 
+    /// @notice unPauses contract transfers callable only by admin
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /// @notice Mints token to the to address
+    /// @param to Address to mint tokens
+    /// @param amount Amount of tokens to be minted
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
+    } 
+    
+    /// @dev token transfer hooks called before every transfer
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
@@ -40,6 +53,7 @@ contract Subsquid is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
         super._beforeTokenTransfer(from, to, amount);
     }
 
+     /// @dev upgrade function called upon during upgrades to the contract
     function _authorizeUpgrade(address newImplementation)
         internal
         onlyOwner
